f5fb06b168ac17ce18cc7aa379302c9cd5037016
MuratYucedag <yucedagmurat23@gmail.com>
2023-10-03 21:36:34 +0300

    ....

--- MyAcademyCarBook.DataAccessLayer/Concrete/CarBookContext.cs
+++ MyAcademyCarBook.DataAccessLayer/Concrete/CarBookContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,17 @@ using System.Threading.Tasks;
 
 namespace MyAcademyCarBook.DataAccessLayer.Concrete
 {
-    internal class CarBookContext
+    public class CarBookContext : DbContext
     {
-        //buradan devam edilecek...
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial catalog=CarBookDb;integrated Security=true");
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarCategory> CarCategories { get; set; }
+        public DbSet<CarStatus> CarStatuses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Price> Prices { get; set; }
     }
 }
