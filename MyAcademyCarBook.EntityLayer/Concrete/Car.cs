f51ca2ec449425f0bafb3ecf2c24d2f5a6fe13c0
MuratYucedag <yucedagmurat23@gmail.com>
2023-11-07 22:40:24 +0300

    ...

--- MyAcademyCarBook.EntityLayer/Concrete/Car.cs
+++ MyAcademyCarBook.EntityLayer/Concrete/Car.cs
@@ -23,5 +23,6 @@ namespace MyAcademyCarBook.EntityLayer.Concrete
         public int CarStatusID { get; set; }
         public CarStatus CarStatus { get; set; }
         public List<Price> Prices { get; set; }
+        public List<CarDetail> CarDetails { get; set; }
     }
 }
--- MyAcademyCarBook.PresentationLayer/Controllers/CarController.cs
+++ MyAcademyCarBook.PresentationLayer/Controllers/CarController.cs
@@ -6,9 +6,11 @@ namespace MyAcademyCarBook.PresentationLayer.Controllers
     public class CarController : Controller
     {
         private readonly ICarService _carService;
-        public CarController(ICarService carService)
+        private readonly ICarDetailService _carDetailService;
+        public CarController(ICarService carService, ICarDetailService carDetailService)
         {
             _carService = carService;
+            _carDetailService = carDetailService;
         }
         public IActionResult Index()
         {
@@ -22,8 +24,18 @@ namespace MyAcademyCarBook.PresentationLayer.Controllers
         }
         public IActionResult CarList()
         {
+            ViewBag.title1 = "Araç Listesi";
+            ViewBag.title2 = "Sizin İçin Araç Listemiz";
             var values=_carService.TGetAllCarsWithBrands();
             return View(values);
+        }
+        public IActionResult CarDetail(int id)
+        {
+            ViewBag.title1 = "Araba Detayları";
+            ViewBag.title2 = "Son Araç Detayları";
+            var value=_carDetailService.TGetCarDetailByCarID(id);
+            ViewBag.v = value.Description;
+            return View();
         }
     }
 }
